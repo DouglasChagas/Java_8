1e49dae5eaf3dc233a86439f2b43efd6118f7817
doug_ <doug_@DC0001>
2017-10-23 17:52:11 -0200

    aula 3

--- src/OrdenaStrings.java
+++ src/OrdenaStrings.java
@@ -3,6 +3,8 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.function.Consumer;
 
+import static java.util.Comparator.*;
+
 public class OrdenaStrings {
 
 	public static void main(String[] args) {
@@ -16,6 +18,9 @@ public class OrdenaStrings {
 		//Comparator<String> comparador = new ComparadorDeStringPorTamanho();
 		//Collections.sort(palavras, comparador);
 		
+		System.out.println(palavras);
+		
+		/*
 		Comparator<String> comparador = new ComparadorDeStringPorTamanho();
 		palavras.sort(comparador);
 		
@@ -23,7 +28,38 @@ public class OrdenaStrings {
 
 		Consumer<String> consumidor = new ConsumidorDeString();
 		palavras.forEach(consumidor);
+		*/
 		
+		//palavras.sort((s1, s2) -> Integer.compare(s1.length(), s2.length()));
+		//palavras.sort(Comparator.comparing(s -> s.length()));
+		//palavras.sort(Comparator.comparing(String::length));
+		
+		//palavras.sort(comparing(String::length));
+		
+//		palavras.sort(Comparator.comparing(s -> s.length()*-1));
+		
+		palavras.sort(String.CASE_INSENSITIVE_ORDER);
+
+		
+		//palavras.forEach(s -> System.out.println(s));
+		palavras.forEach(System.out::println);
+		
+		//Object o = s -> System.out.println(s);
+/*
+		new Thread(() -> System.out.println("Executando um Runnable")).start();
+		
+		//new Thread(() -> System.out.println("Executando um Runnable")).start();
+
+		
+		new Thread(new Runnable() {
+
+		    @Override
+		    public void run() {
+		        System.out.println("Executando um Runnable");
+		    }
+
+		}).start();
+*/
 	}
 
 }
